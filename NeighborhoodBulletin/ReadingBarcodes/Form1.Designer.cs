5d5b454d16aeb5aa14ec618edb0e99a426572e6c
cascando <mmittelstedt@icloud.com>
2019-08-19 12:58:57 -0500

    added logic for Reading Barcodes form

--- NeighborhoodBulletin/ReadingBarcodes/Form1.Designer.cs
+++ NeighborhoodBulletin/ReadingBarcodes/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadImageButton = new System.Windows.Forms.Button();
+            this.readBarcodesButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loadImageButton
+            // 
+            this.loadImageButton.Location = new System.Drawing.Point(346, 165);
+            this.loadImageButton.Name = "loadImageButton";
+            this.loadImageButton.Size = new System.Drawing.Size(127, 23);
+            this.loadImageButton.TabIndex = 0;
+            this.loadImageButton.Text = "Load image";
+            this.loadImageButton.UseVisualStyleBackColor = true;
+            this.loadImageButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // readBarcodesButton
+            // 
+            this.readBarcodesButton.Location = new System.Drawing.Point(346, 222);
+            this.readBarcodesButton.Name = "readBarcodesButton";
+            this.readBarcodesButton.Size = new System.Drawing.Size(127, 23);
+            this.readBarcodesButton.TabIndex = 1;
+            this.readBarcodesButton.Text = "Read barcodes";
+            this.readBarcodesButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.readBarcodesButton);
+            this.Controls.Add(this.loadImageButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadImageButton;
+        private System.Windows.Forms.Button readBarcodesButton;
     }
 }
 
--- NeighborhoodBulletin/ReadingBarcodes/Form1.cs
+++ NeighborhoodBulletin/ReadingBarcodes/Form1.cs
@@ -6,15 +6,125 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Leadtools.Codecs;
+using Leadtools.Forms.Common;
+using Leadtools;
+using Leadtools.Barcode;
 using System.Windows.Forms;
 
 namespace ReadingBarcodes
 {
     public partial class Form1 : Form
     {
+        private BarcodeEngine barcodeEngineInstance; // The barcode engine 
+        private RasterImage theImage; // The current loaded image 
+        private string imageFileName; // Last file name we loaded; this is used in the "Writing Barcodes" tutorial 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            // Requires a license file that unlocks 1D barcode read functionality. 
+            string MY_LICENSE_FILE = @"C:\LEADTOOLS 20\Common\License\leadtools.lic";
+            string MY_DEVELOPER_KEY = System.IO.File.ReadAllText(@"C:\LEADTOOLS 20\Common\License\leadtools.lic.key");
+            RasterSupport.SetLicense(MY_LICENSE_FILE, MY_DEVELOPER_KEY);
+
+            // Create the BarcodeEngine instance 
+            barcodeEngineInstance = new BarcodeEngine();
+
+            readBarcodesButton.Click += readBarcodesButton_Click;
+            loadImageButton.Click += loadImageButton_Click;
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            // Dispose of the image 
+            if (theImage != null)
+            {
+                theImage.Dispose();
+            }
+
+            base.OnFormClosed(e);
+        }
+
+        private void loadImageButton_Click(object sender, EventArgs e)
+        {
+            string fileName = @"C:\Users\Public\Documents\LEADTOOLS Images\Barcode1.tif";
+            // Or uncomment the following to load your own file 
+            //using(OpenFileDialog dlg = new OpenFileDialog()) 
+            //{ 
+            //   if(dlg.ShowDialog(this) == System.Windows.Forms.DialogResult.OK) 
+            //   { 
+            //      fileName = dlg.FileName; 
+            //   } 
+            //   else 
+            //   { 
+            //      return; 
+            //   } 
+            //} 
+
+            // Load the image 
+            using (RasterCodecs codecs = new RasterCodecs())
+            {
+                RasterImage newImage = codecs.Load(fileName, 0, CodecsLoadByteOrder.BgrOrGray, 1, 1);
+
+                // Dispose of the image 
+                if (theImage != null)
+                {
+                    theImage.Dispose();
+                }
+
+                theImage = newImage;
+                imageFileName = fileName;
+            }
+            MessageBox.Show("Image loaded");
+        }
+
+        private void readBarcodesButton_Click(object sender, EventArgs e)
+        {
+            if (theImage == null)
+            {
+                loadImageButton_Click(sender, e);
+            }
+
+            try
+            {
+                // Read all the barcodes 
+                // The first parameter is the image from which to read the barcodes. 
+                // The second parameter is the search rectangle. Pass an empty rectangle to search the entire image. 
+                // The third parameter is the maximum number of barcodes to read. Pass 0 for all found in the image. 
+                // The last parameter is an array of the BarcodeSymbology that we are interested in finding. Pass null (or Nothing) 
+                // to find all available barcodes found in the image and supported by the current license. 
+                BarcodeData[] dataArray = barcodeEngineInstance.Reader.ReadBarcodes(theImage, LeadRect.Empty, 0, null);
+
+                StringBuilder sb = new StringBuilder();
+                sb.AppendFormat("{0} barcode(s) found", dataArray.Length);
+                sb.AppendLine();
+
+                for (int i = 0; i < dataArray.Length; i++)
+                {
+                    BarcodeData data = dataArray[i];
+
+                    sb.AppendFormat("Symbology: {0}, Location: {1}, Data: {2}", data.Symbology.ToString(), data.Bounds.ToString(), data.Value);
+                    sb.AppendLine();
+                }
+
+                MessageBox.Show(sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
